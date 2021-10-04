5aee6faa0c757d349838c47e99ea6d3a2f1e2785
Damien Delbreil <damien@lescommis.com>
2021-10-04 18:55:16 +0200

    update end day

--- src/app.js
+++ src/app.js
@@ -1,37 +1,54 @@
 const Koa = require('koa')
+
+// acceder aux params depuis le contexte
 const koaBody = require('koa-body')
+
+// declarer un router pour avoir plusieurs route
 const Router = require('koa-router')
 
 // Declare Main Application
 const app = module.exports = new Koa()
 app.use(koaBody({ multipart: true }))
 
+// Declare Router
 var router = new Router()
 
+// add route get to Router
 router.get('/hello-world/', (ctx, next) => {
-    console.log(ctx)
     ctx.body = 'Hello-World'
 })
 
-router.post('/body-parser-object/', (ctx, next) => {
-    ctx.body = ctx.request.body
-})
-
-router.put('/body-parser-object/:id', (ctx, next) => {
-    ctx.request.body.id = ctx.params.id
-    ctx.body = ctx.request.body
-})
-
-router.post('/body-parser-string/', (ctx, next) => {
-    ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
-})
-
-app.use(router.routes())
-
-// demarrer l'application
+// Start Application
 if (!module.parent) {
     app
-        .use(router.routes())
-        .use(router.allowedMethods())
-        .listen(3000)
+    .use(router.routes()) // Specify we use a Router
+    .use(router.allowedMethods()) 
+    .listen(3000)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// router.post('/body-parser-object/', (ctx, next) => {
+//     ctx.body = ctx.request.body
+// })
+
+// router.put('/body-parser-object/:id', (ctx, next) => {
+//     ctx.request.body.id = ctx.params.id
+//     ctx.body = ctx.request.body
+// })
+
+// router.post('/body-parser-string/', (ctx, next) => {
+//     ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
+// })
+
