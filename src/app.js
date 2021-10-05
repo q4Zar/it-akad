607e32dba5738e889ddd77afdfd7afe84e994ba7
Damien Delbreil <damien@lescommis.com>
2021-10-05 11:57:22 +0200

    completer les deux routes authentification-to-db et get-db-collection

--- src/app.js
+++ src/app.js
@@ -18,37 +18,44 @@ router.get('/hello-world/', (ctx, next) => {
     ctx.body = 'Hello-World'
 })
 
+router.post('/body-parser-object/', (ctx, next) => {
+    ctx.body = ctx.request.body
+})
+
+router.put('/body-parser-object/:id', (ctx, next) => {
+    ctx.request.body.id= ctx.params.id
+    ctx.body = ctx.request.body
+})
+
+//
+router.post('/body-parser-string/', (ctx, next) => {
+    ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
+})
+
+router.post('/authentification-to-db',  (ctx, next) => {
+    // a vous de coder
+    // return jwt
+})
+
+router.post('/get-db-collection', (ctx, next) => {
+    // recuperer en parametre le nom de la collection a requeter
+    // a vous de coder
+    // return content of collection
+})
+
+
+router.post('/',  (ctx, next) => {
+    // a vous de coder
+    return {jwt: jwt}
+})
+
+
+
 // Start Application
 if (!module.parent) {
     app
     .use(router.routes()) // Specify we use a Router
-    .use(router.allowedMethods()) 
+    .use(router.allowedMethods())
     .listen(3000)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// router.post('/body-parser-object/', (ctx, next) => {
-//     ctx.body = ctx.request.body
-// })
-
-// router.put('/body-parser-object/:id', (ctx, next) => {
-//     ctx.request.body.id = ctx.params.id
-//     ctx.body = ctx.request.body
-// })
-
-// router.post('/body-parser-string/', (ctx, next) => {
-//     ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
-// })
 
---
+++ src/auth.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+// requete une la base de donnes avec login + password afin de recuperer un token (jwt : json web token)
+axios.post('https://gql.alcyone.life/auth/local', {
+        identifier: 'itakad@gmail.com',
+        password: 'itakad2020',
+    })
+    .then(function (response) {
+
+        console.log(response.data) // handle succes
+
+        // recuperer jwt pour joindre a une requete authentifier
+        let jwt = response.data.jwt
+
+        // construire bearer auth token a joindre avec la requete
+        console.log(`Bearer ${response.data.jwt}`)
+
+        // recuperer la liste des categories disponibles en base de donnees
+        axios.get('https://gql.alcyone.life/categories',
+            { headers: { Authorization: `Bearer ${jwt}` }
+        }).then(function (response) {
+            console.log(response.data)
+        })
+        .catch(function (error) {
+            console.log('error') // handle error
+        })
+        .then(function () {
+        console.log('--- --- ---')
+        })
+
+    })
+    .catch(function (error) {
+        console.log('error') // handle error
+    })
+    .then(function () {
+        console.log('--- --- ---')
+    });
