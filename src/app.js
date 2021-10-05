cffb387dd1563b7bc29a29aa633a3998557a8914
Damien Delbreil <damien@lescommis.com>
2021-10-05 13:59:34 +0200

    request to auth db

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const Koa = require('koa')
 
 // acceder aux params depuis le contexte
@@ -32,13 +33,41 @@ router.post('/body-parser-string/', (ctx, next) => {
     ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
 })
 
-router.post('/authentification-to-db',  (ctx, next) => {
-    // a vous de coder
-    // return jwt
+router.post('/authentification-to-db',  async (ctx, next) => {
+    
+    // on recuperer les elements distincts
+    // ctx.request.body = { identifier: 'itakad@gmail.com', password: 'itakad2020' }
+    let login = ctx.request.body.login
+    let password = ctx.request.body.password
+
+    // declare une variable pour le json web token
+    var jwt = null
+
+    // si les login et password sont correct je recupere un jwt
+    await axios.post('https://gql.alcyone.life/auth/local', {
+        identifier: login,
+        password: password,
+    })
+    .then(response =>{
+        console.log(response.data) // handle succes
+        // extraction du jwt de la reponse
+        jwt = response.data.jwt
+    })
+    .catch(function (error) {
+        console.log('error') // handle error
+    })
+    .then(function () {
+        console.log('--- --- ---')
+    });
+    // affichage jwt
+    console.log(jwt)
+    // je retourne un object avec le jwt
+    ctx.body = {jwt: jwt}
 })
 
 router.post('/get-db-collection', (ctx, next) => {
     // recuperer en parametre le nom de la collection a requeter
+    // ensuite requeter la base de donnee
     // a vous de coder
     // return content of collection
 })
--- src/client.js
+++ src/client.js
@@ -1,50 +1,77 @@
 const axios = require('axios');
 
 // get hello-world
-axios.get('http://localhost:3000/hello-world/')
-  .then(function (response) {
-    console.log(response.data) // handle succes
-  })
-  .catch(function (error) {
-    console.log('error') // handle error
-  })
-  .then(function () {
-    console.log('--- --- ---')
-  });
+// axios.post('http://localhost:3000/get-db-collection/')
+//   .then(function (response) {
+//     console.log(response.data) // handle succes
+//   })
+//   .catch(function (error) {
+//     console.log('error') // handle error
+//   })
+//   .then(function () {
+//     console.log('--- --- ---')
+//   });
 
 
 
-// parse object
-  axios.post('http://localhost:3000/body-parser-object/', {login: 'test', password: 'pwd' })
-    .then(function (response) { 
-      console.log(response.data) // handle succes
-    })
-    .catch(function (error) { 
-      console.log(error) // handle error
-    })
-    .then(function () {
-      console.log('--- --- ---')
-    });
+// // parse object
+//   axios.post('http://localhost:3000/body-parser-object/', {login: 'test', password: 'pwd' })
+//     .then(function (response) { 
+//       console.log(response.data) // handle succes
+//     })
+//     .catch(function (error) { 
+//       console.log(error) // handle error
+//     })
+//     .then(function () {
+//       console.log('--- --- ---')
+//     });
 
 
 
 
 
-// parse object and add params :id
-axios.put('http://localhost:3000/body-parser-object/1', {login: 'test', password: 'pwd' })
-  .then(function (response) { 
-    console.log(response.data) // handle succes
-  })
-  .catch(function (error) { 
-    console.log(error) // handle error
-  })
-  .then(function () {
-    console.log('--- --- ---')
-  });
+// // parse object and add params :id
+// axios.put('http://localhost:3000/body-parser-object/1', {login: 'test', password: 'pwd' })
+//   .then(function (response) { 
+//     console.log(response.data) // handle succes
+//   })
+//   .catch(function (error) { 
+//     console.log(error) // handle error
+//   })
+//   .then(function () {
+//     console.log('--- --- ---')
+//   });
 
 
 // parse object and convert to string
-axios.post('http://localhost:3000/body-parser-string/', {login: 'test', password: 'pwd' })
+// axios.post('http://localhost:3000/body-parser-string/', {login: 'test', password: 'pwd' })
+//   .then(function (response) {// handle succes
+//     console.log(response.data);
+//   })
+//   .catch(function (error) { // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     console.log('--- --- ---')
+//   });
+
+
+// parse object and convert to string
+// axios.post('http://localhost:3000/authentification-to-db/', 
+//   {login: 'itakad@gmail.com', password: 'itakad2020' })
+//   .then(function (response) {// handle succes
+//     console.log(response.data);
+//   })
+//   .catch(function (error) { // handle error
+//     console.log('error')
+//     // console.log(error);
+//   })
+//   .then(function () {
+//     console.log('--- --- ---')
+//   });
+
+// // parse object and convert to string
+axios.post('http://localhost:3000/authentification-to-db/', {login: 'itakad@gmail.com', password: 'itakad2020' })
   .then(function (response) {// handle succes
     console.log(response.data);
   })
