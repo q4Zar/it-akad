9c7ba9c41b78f2e431498929153971905e346189
Damien Delbreil <damien@lescommis.com>
2021-10-07 12:38:13 +0200

    auth.js

--- back/tests/auth.js
+++ back/tests/auth.js
@@ -1,10 +1,34 @@
 const axios = require('axios');
 
 // requete une la base de donnes avec login + password afin de recuperer un token (jwt : json web token)
-//
 axios.post('https://gql.alcyone.life/auth/local', { identifier: 'itakad@gmail.com', password: 'itakad2020' })
     .then(function (response) {
-        console.log(response.data) // handle succes
+        // console.log(response.data) // handle succes
+        // recuperer jwt pour joindre a une requete authentifier
+        let jwt = response.data.jwt
+        // construire bearer auth token a joindre avec la requete
+        console.log(`Bearer ${response.data.jwt}`)
+        // recuperer la liste des categories disponibles en base de donnees
+        axios.get('https://gql.alcyone.life/Blog-Articles', { headers: { Authorization: `Bearer ${jwt}` }
+            }).then(function (response) {
+                console.log(response.data)
+            })
+            .catch(function (error) {
+                console.log('error') // handle error
+            })
+            .then(function () {
+                console.log('--- --- ---')
+            })
+            axios.get('https://gql.alcyone.life/Blog-Comments', { headers: { Authorization: `Bearer ${jwt}` }
+            }).then(function (response) {
+                console.log(response.data)
+            })
+            .catch(function (error) {
+                console.log('error') // handle error
+            })
+            .then(function () {
+                console.log('--- --- ---')
+            })
     })
     .catch(function (error) {
         // console.log(error)
@@ -14,35 +38,29 @@ axios.post('https://gql.alcyone.life/auth/local', { identifier: 'itakad@gmail.co
         console.log('--- --- ---')
     });
 
-//
-axios.get('https://gql.alcyone.life/test?name=Damien')
-.then(function (response) {
-    console.log(response.data) // handle succes
-})
-.catch(function (error) {
-    // console.log(error)
-    console.log('error') // handle error
-})
-.then(function () {
-    console.log('--- --- ---')
-});
 
 
-// recuperer jwt pour joindre a une requete authentifier
-        // let jwt = response.data.jwt
 
-        // // construire bearer auth token a joindre avec la requete
-        // console.log(`Bearer ${response.data.jwt}`)
 
-        // // recuperer la liste des categories disponibles en base de donnees
-        // axios.get('https://gql.alcyone.life/categories',
-        //     { headers: { Authorization: `Bearer ${jwt}` }
-        // }).then(function (response) {
-        //     console.log(response.data)
-        // })
-        // .catch(function (error) {
-        //     console.log('error') // handle error
-        // })
-        // .then(function () {
-        // console.log('--- --- ---')
-        // })
+
+// //
+// axios.get('https://gql.alcyone.life/test?name=Damien')
+// .then(function (response) {
+//     console.log(response.data) // handle succes
+// })
+// .catch(function (error) {
+//     // console.log(error)
+//     console.log('error') // handle error
+// })
+// .then(function () {
+//     console.log('--- --- ---')
+// });
+
+
+// // recuperer jwt pour joindre a une requete authentifier
+// let jwt = response.data.jwt
+
+// // construire bearer auth token a joindre avec la requete
+// console.log(`Bearer ${response.data.jwt}`)
+
+
